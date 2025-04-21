1c91f91bd24c1f396dbe22ad6ce1fc653cf8bb2f
hayleegraham <graham.haylee@yahoo.com>
2025-04-21 14:27:44 -0700

    first build with 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      new URL(
-        "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTdhYWV4MDdrOGY3eWloeTVzamR3ZnUwZnBycDd3ZzhkZHN2ZnZ2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26BGwSs39WPuxsFhe/giphy.gif"
-      ),
-    ],
+    unoptimized: true,
+    // remotePatterns: [
+    //   new URL(
+    //     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTdhYWV4MDdrOGY3eWloeTVzamR3ZnUwZnBycDd3ZzhkZHN2ZnZ2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26BGwSs39WPuxsFhe/giphy.gif"
+    //   ),
+    // ],
   },
 };
 
